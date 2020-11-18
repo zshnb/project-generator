1a14cef4660a2460b9095a27541346435ce8195f
zshnb <a857681664@gmail.com>
2020-11-18 08:55:44 +0800

    set allow origin, delete zip

--- src/main/kotlin/com/zshnb/projectgenerator/web/config/GlobalCorsConfig.kt
+++ src/main/kotlin/com/zshnb/projectgenerator/web/config/GlobalCorsConfig.kt
@@ -10,7 +10,7 @@ class GlobalCorsConfig {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**") //添加映射路径，“/**”表示对所有的路径实行全局跨域访问权限的设置
-                    .allowedOrigins("http://localhost:8080") //开放哪些ip、端口、域名的访问权限
+                    .allowedOriginPatterns("*") //开放哪些ip、端口、域名的访问权限
                     .allowCredentials(true) //是否允许发送Cookie信息
                     .allowedMethods("GET", "POST", "PUT", "DELETE") //开放哪些Http方法，允许跨域访问
                     .allowedHeaders("*") //允许HTTP请求中的携带哪些Header信息
