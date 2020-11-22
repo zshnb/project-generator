1ff9ef1f3af1aca20732d574d3a053238aecb525
zshnb <a857681664@gmail.com>
2020-11-22 13:57:30 +0800

    add id

--- src/test/kotlin/com/zshnb/projectgenerator/generator/GeneratorTest.kt
+++ src/test/kotlin/com/zshnb/projectgenerator/generator/GeneratorTest.kt
@@ -1,15 +1,12 @@
 package com.zshnb.projectgenerator.generator
 
-import com.google.gson.Gson
 import com.zshnb.projectgenerator.generator.generator.LayuiGenerator
 import org.apache.commons.io.FileUtils
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ClassPathResource
-import org.springframework.util.ResourceUtils
 import java.io.File
-import java.net.URL
 import java.nio.charset.StandardCharsets
 
 @SpringBootTest
