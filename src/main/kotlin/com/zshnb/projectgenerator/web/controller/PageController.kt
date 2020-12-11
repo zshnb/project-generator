008bb493f23bc5e128e44a9fa9764756381c8c2a
zshnb <a857681664@gmail.com>
2020-12-11 19:27:50 +0800

    create FormItemType to be return data

--- src/main/kotlin/com/zshnb/projectgenerator/web/controller/PageController.kt
+++ src/main/kotlin/com/zshnb/projectgenerator/web/controller/PageController.kt
@@ -1,18 +1,22 @@
 package com.zshnb.projectgenerator.web.controller
 
-import com.zshnb.projectgenerator.generator.entity.FormItem
+import com.zshnb.projectgenerator.generator.entity.*
 import com.zshnb.projectgenerator.web.common.ListResponse
-import org.reflections.Reflections
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/page")
 class PageController {
+    private val formItemTypes = listOf(
+        FormItemType("输入框", InputFormItem::class.qualifiedName!!),
+        FormItemType("下拉选择框", SelectFormItem::class.qualifiedName!!),
+        FormItemType("单选框", RadioFormItem::class.qualifiedName!!),
+        FormItemType("文本域", TextAreaFormItem::class.qualifiedName!!)
+    )
     @GetMapping("/form-items")
-    fun listFormItems(): ListResponse<String> {
-        val kclass = FormItem::class.java
-        val reflections = Reflections("com.zshnb.projectgenerator.generator.entity")
-        val types = reflections.getSubTypesOf(kclass)
-        return ListResponse.ok(types.map { it.name })
+    fun listFormItems(): ListResponse<FormItemType> {
+        return ListResponse.ok(formItemTypes)
     }
+
+    data class FormItemType(val name: String, val className: String)
 }
