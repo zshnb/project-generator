b125b9bff234a8607d17cad020862cf214c83985
zshnb <a857681664@gmail.com>
2021-03-09 12:40:05 +0800

    create table component

--- src/main/kotlin/com/zshnb/projectgenerator/generator/entity/Page.kt
+++ src/main/kotlin/com/zshnb/projectgenerator/generator/entity/Page.kt
@@ -5,17 +5,35 @@ package com.zshnb.projectgenerator.generator.entity
  * @param entity 页面所代表的实体
  * */
 class Page(val entity: Entity?,
-           val form: FormComponent) {
+           val form: FormComponent,
+           val table: TableComponent) {
 }
 
+/**
+ * 表单组件
+ * */
 class FormComponent(val formItems: List<FormItem>)
+
+/**
+ * 表格组建
+ * */
+class TableComponent(val fields: List<TableField>)
 
 /**
  * 表格的列
  * @param field 列对应实体的属性域
- * @param title 列在表格页面上显示的列描述
+ * @param title 列在表格页面上显示的描述
  * */
-data class TableField(val title: String, val field: Field)
+data class TableField(val title: String,
+                      val field: Field?,
+                      val mapping: Mapping?)
+
+/**
+ * 表格列值与显示内容的映射
+ * @param source 值，存在数据库里的数据
+ * @param target 内容，显示在网页上的数据
+ * */
+data class Mapping(val source: Any, val target: Any)
 
 open class FormItem(val field: Field?,
                     val formItemClassName: String,
