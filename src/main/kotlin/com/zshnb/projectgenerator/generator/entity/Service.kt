6e332637890c7d0c11aa7e7a57e0e81dff6abcb0
zheng sihua <a857681664@gmail.com>
2021-05-16 22:50:59 +0800

    feat: support column no repeat (#46)
    * add property
    
    * add no repeat logic
    
    * modify comment

--- src/main/kotlin/com/zshnb/projectgenerator/generator/entity/Service.kt
+++ src/main/kotlin/com/zshnb/projectgenerator/generator/entity/Service.kt
@@ -3,5 +3,4 @@ package com.zshnb.projectgenerator.generator.entity
 class Service(val packageName: String,
               val entity: Entity,
               val implPackageName: String,
-              val dependencies: List<String>) {
-}
+              val dependencies: List<String>)
