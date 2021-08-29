372286a84246aafe732bbeb8137e80f948671401
zshnb <a857681664@gmail.com>
2021-08-29 15:39:33 +0800

    fix: add listFieldTypes for c project

--- src/main/kotlin/com/zshnb/projectgenerator/web/controller/ColumnController.kt
+++ src/main/kotlin/com/zshnb/projectgenerator/web/controller/ColumnController.kt
@@ -1,16 +1,20 @@
 package com.zshnb.projectgenerator.web.controller
 
+import com.zshnb.projectgenerator.generator.entity.c.FieldType
 import com.zshnb.projectgenerator.web.common.ListResponse
 import com.zshnb.projectgenerator.web.serviceImpl.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/column")
+@RequestMapping("/api")
 class ColumnController {
     @Autowired
     private lateinit var columnServiceImpl: ColumnServiceImpl
 
-    @GetMapping("/types")
+    @GetMapping("/column/types")
     fun listColumnTypes(): ListResponse<String> = ListResponse.ok(columnServiceImpl.listColumnType())
+
+    @GetMapping("/field/types")
+    fun listCFieldTypes(): ListResponse<String> = ListResponse.ok(FieldType.values().map { it.description })
 }
